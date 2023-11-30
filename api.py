5cb23dbef4d02c168a72b5a9e48ddda895f6ccfa
Milad Tehrani <tehranimilad92@gmail.com>
2023-11-30 15:20:29 -0800

    updated and ran python API script

--- api.py
+++ api.py
@@ -1,15 +1,17 @@
-# from serpapi import GoogleSearch
+from serpapi import GoogleSearch
 
-# params = {
-#   "api_key": "cba426906bb6d8e1c3f137c92f72a3cfb2fb6de4ecd9e91293b531caa94078c8",
-#   "engine": "google",
-#   "q": "Latest Cryptocurrency News",
-#   "google_domain": "google.com",
-#   "gl": "us",
-#   "hl": "en",
-#   "safe": "off",
-#   "tbm": "nws"
-# }
+params = {
+  "api_key": "cba426906bb6d8e1c3f137c92f72a3cfb2fb6de4ecd9e91293b531caa94078c8",
+  "engine": "google",
+  "q": "Cryptocurrency",
+  "google_domain": "google.com",
+  "gl": "us",
+  "hl": "en",
+  "safe": "off",
+  "start": "0",
+  "tbm": "nws"
+}
 
-# search = GoogleSearch(params)
-# results = search.get_dict()
+search = GoogleSearch(params)
+results = search.get_dict()
+print(results)
